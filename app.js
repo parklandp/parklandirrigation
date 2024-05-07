e3eed077249df0ed84540aa54a2aea3a32747502
parklandp <joseph.waddingham@parkland.co.nz>
2024-05-08 11:08:52 +1200

    t

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
-});
+// Serve static files from the "public" directory
+app.use(express.static('public'));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
